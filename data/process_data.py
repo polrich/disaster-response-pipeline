82e6130b0866191d481114bd63c8ca17892620c4
polrich <135112471+polrich@users.noreply.github.com>
2023-07-11 10:28:14 +0200

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -73,7 +73,7 @@ def save_data(df, database_filename):
     '''
     # Save the clean dataset into an sqlite database
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('DisasterResponse', engine, index=False)
+    df.to_sql('DisasterResponse', con=engine, if_exists='replace', index=False)
 
 
 def main():
